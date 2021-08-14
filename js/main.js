4a712e346c366ad5c34106b9a1e86a67d0346ab5
loitez <87999189+loitez@users.noreply.github.com>
2021-08-15 02:27:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $(function(){
         prevArrow: '<img src="images/arrow-left.svg" alt="" class="slider__arrow slider__arrow-left">',
         nextArrow: '<img src="images/arrow-right.svg" alt="" class="slider__arrow slider__arrow-right">',
     });
+    $('.menu-btn').on('click', function () {
+        $('.menu__list').toggleClass('active')
+    })
 });
