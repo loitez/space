757d63851b18f51114c75a427cddce865b8cb32f
loitez <87999189+loitez@users.noreply.github.com>
2021-08-15 17:26:58 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,8 +2,24 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img src="images/arrow-left.svg" alt="" class="slider__arrow slider__arrow-left">',
         nextArrow: '<img src="images/arrow-right.svg" alt="" class="slider__arrow slider__arrow-right">',
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1205,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+    ],
+
+
+
     });
     $('.menu-btn').on('click', function () {
         $('.menu__list').toggleClass('active')
     })
+    $("#slick-slide-control00").html("");
+    $("#slick-slide-control01").html("");
+    $("#slick-slide-control02").html("");
 });
