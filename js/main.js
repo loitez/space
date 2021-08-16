56007dfba35808ddb9d41b3bbdac73247c5bccf7
loitez <87999189+loitez@users.noreply.github.com>
2021-08-16 22:37:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,3 +23,31 @@ $(function(){
     $("#slick-slide-control01").html("");
     $("#slick-slide-control02").html("");
 });
+
+(function() {
+    'use strict';
+
+    function trackScroll() {
+        var scrolled = window.pageYOffset;
+        var coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            goTopBtn.classList.add('back_to_top-show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('back_to_top-show');
+        }
+    }
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -50);
+            setTimeout(backToTop, 0);
+        }
+    }
+
+    var goTopBtn = document.querySelector('.back_to_top');
+
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+})();
